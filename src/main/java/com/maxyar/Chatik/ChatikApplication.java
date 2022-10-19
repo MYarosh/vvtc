6eb310fe2d0a311925ef445afa06bee70c2d7db6
MYarosh <maximyarosh@mail.ru>
2022-10-19 12:37:20 +0300

    Initial commit

---
+++ src/main/java/com/maxyar/Chatik/ChatikApplication.java
@@ -0,0 +1,11 @@
+package com.maxyar.Chatik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatikApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatikApplication.class, args);
+    }
+}
